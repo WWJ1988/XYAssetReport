295cbac6af46d29cef53a8c14ff51765672f32f1
Neil Wang <neilwang@Objectivasoftware.com>
2017-02-22 09:52:21 +0800

    no message

--- a2-test/src/systemjs.config.js
+++ a2-test/src/systemjs.config.js
@@ -16,8 +16,8 @@
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
 
             // other libraries
-            'rxjs': 'npm:rxjs',
-            'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+            'rxjs': 'npm:rxjs'
+            // 'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
@@ -31,6 +31,12 @@
                 defaultExtension: 'js'
             },
             XYAssetReport: {
+                defaultExtension: 'js'
+            },
+            "app.module": {
+                defaultExtension: 'js'
+            },
+            "app.component": {
                 defaultExtension: 'js'
             }
         }
