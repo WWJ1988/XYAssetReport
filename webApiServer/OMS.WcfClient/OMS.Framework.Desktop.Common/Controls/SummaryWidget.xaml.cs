65f534120f7e7659f9d3f5046d735f9901c3c33f
WWJ1988 <1004338903@qq.com>
2017-03-30 21:08:29 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    
    #	webApiServer/OMS.WcfClient/OMS.Framework.Desktop/Controls/FrameworkSummary.xaml
    
    #	webApiServer/OMS.WcfClient/OMS.Trading.Desktop.Order/Summaries/OrderSumamry/OrderSummaryWidget.xaml
    
    #	webApiServer/OMS.WcfClient/OMS.Trading.Desktop.Order/Summaries/OrderSumamry/OrderSummaryWidgetViewModel.cs
    
    litttle change

--- webApiServer/OMS.WcfClient/OMS.Framework.Desktop.Common/Controls/SummaryWidget.xaml.cs
+++ webApiServer/OMS.WcfClient/OMS.Framework.Desktop.Common/Controls/SummaryWidget.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace OMS.Framework.Desktop.Common.Controls
 {
@@ -7,9 +8,24 @@ namespace OMS.Framework.Desktop.Common.Controls
     /// </summary>
     public partial class SummaryWidget : UserControl
     {
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+           "Title", typeof(string), typeof(SummaryWidget), new FrameworkPropertyMetadata());
+
         public SummaryWidget()
         {
             InitializeComponent();
+        }
+
+        public string Title
+        {
+            get
+            {
+                return (string)GetValue(TitleProperty);
+            }
+            set
+            {
+                SetValue(TitleProperty, value);
+            }
         }
     }
 }
--- webApiServer/OMS.WcfClient/OMS.Trading.Desktop.Order/Summaries/OrderSumamry/OrderSummaryWidgetViewModel.cs
+++ webApiServer/OMS.WcfClient/OMS.Trading.Desktop.Order/Summaries/OrderSumamry/OrderSummaryWidgetViewModel.cs
@@ -7,12 +7,19 @@ using OMS.Framework.Desktop.Common.Controls;
 
 namespace OMS.Trading.Desktop.Order.Summaries.OrderSumamry
 {
-	public class OrderSummaryWidgetViewModel : SummaryWidgetBaseViewModel
-	{
-		private const string title = "P/L Overview";
+    public class OrderSummaryWidgetViewModel : SummaryWidgetBaseViewModel
+    {
+        private const string title = "P/L Overview";
+        private string summary = "P/L Overview";
 
-		public OrderSummaryWidgetViewModel()
-			:base(title)
-		{ }
-	}
+        public OrderSummaryWidgetViewModel()
+            : base(title)
+        { }
+
+        public string Summary
+        {
+            get { return summary; }
+            set { SetProperty(ref summary, value); }
+        }
+    }
 }
